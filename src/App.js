670a17a2bc0314097366b554711785a4d6bfe3b2
abdulaziz899 <abdulaziz0170934@gmail.com>
2021-03-15 11:19:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.Config";
+import { useState } from "react";
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 function App() {
+  const [users, setUser] = useState({});
+  const GoogleProvider = new firebase.auth.GoogleAuthProvider();
+  var facebookProvider = new firebase.auth.FacebookAuthProvider();
+  var provider = new firebase.auth.GithubAuthProvider();
+  const GoogleSignIn = () => {
+    console.log("object");
+    firebase
+      .auth()
+      .signInWithPopup(GoogleProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log(user, token);
+        setUser(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  };
+
+  const FacebookSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(facebookProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var user = result.user;
+        var accessToken = credential.accessToken;
+        console.log("fb", user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  };
+  const ghSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = credential.accessToken;
+
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        console.log(user);
+        setUser(user)
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={GoogleSignIn}>Google SignIn</button> <br /> <br />
+      <button onClick={FacebookSignIn}>facebook SignIn</button> <br /> <br />
+      <button onClick={ghSignIn}>GitHub SignIn</button> <br /> <br />
+      <p>{users.displayName}</p>
+      <p>{users.email}</p>
     </div>
   );
 }
---
+++ src/firebase.Config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkL2n_IXM0dVc0poKtI7tH7XgnZ5ZhZtc",
+  authDomain: "practices-app-15026.firebaseapp.com",
+  projectId: "practices-app-15026",
+  storageBucket: "practices-app-15026.appspot.com",
+  messagingSenderId: "286813964464",
+  appId: "1:286813964464:web:59fae13f2a37eb7fce5375"
+};
+export default firebaseConfig;
